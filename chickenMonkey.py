b27dcc70ac6eece1e13d810e5281f2ef034197a2
James Chapman <chapmanjamesg@gmail.com>
2020-01-16 11:15:30 -0600

    first commit

---
+++ chickenMonkey.py
@@ -0,0 +1,12 @@
+for i in range(1, 101):
+    if ( i % 5 == 0 & i % 7 == 0):
+        print("chicken monkey")
+    elif (i % 7 == 0):
+        print("monkey")
+
+    elif( i % 5 == 0):
+        print("chicken")
+
+    else:
+         print(i)
+
